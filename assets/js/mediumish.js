25866381dbc55c93d9798aaa86b071f836729c00
bing.gu <bing.gu@toss.im>
2021-01-10 22:56:06 +0900

    modify about

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -104,7 +104,7 @@ jQuery(document).ready(function($){
         lastScrollTop = st;
     }
         
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');  
+   // $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
     
     // spoilers
      $(document).on('click', '.spoiler', function() {
